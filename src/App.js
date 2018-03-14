2faa5d1067440455e22d887c36e5e723c4fe79b5
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-14 16:43:50 +0700

    onClick

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import './App.css';
 import { Word } from './Word'; // { Word: Word }
 import { ListWord } from './ListWord';
 
+function show() {
+  alert(123);
+}
+
 class App extends Component {
   render() {
     const word1 = { en: 'One', vn: 'Mot', isMemorized: true };
     const word2 = { en: 'Two', vn: 'Hai', isMemorized: false };
+    const words = ['NodeJS', 'ReactJS', 'Angular'];
     return (
       <div className="App">
         <ListWord title="English - Vietnamese" isShowForm={false} />
+        <button className="btn btn-success" onClick={show}>
+          Show Text
+        </button>
         <Word wordInfo={word1} />
         <Word wordInfo={word2} />
+        { words.map(w => <p>{w}</p>) }
       </div>
     );
   }
