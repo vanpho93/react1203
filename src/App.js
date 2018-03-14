8f3f5795a5800a6f334a3cb954104d0da115e16c
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-14 16:49:30 +0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Word } from './Word'; // { Word: Word }
-import { ListWord } from './ListWord';
-
-function show() {
-  alert(123);
-}
-
+// state
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { num: 10 };
+  }
   render() {
-    const word1 = { en: 'One', vn: 'Mot', isMemorized: true };
-    const word2 = { en: 'Two', vn: 'Hai', isMemorized: false };
-    const words = ['NodeJS', 'ReactJS', 'Angular'];
     return (
-      <div className="App">
-        <ListWord title="English - Vietnamese" isShowForm={false} />
-        <button className="btn btn-success" onClick={show}>
-          Show Text
-        </button>
-        <Word wordInfo={word1} />
-        <Word wordInfo={word2} />
-        { words.map(w => <p>{w}</p>) }
+      <div className="container">
+        <h3>Value: {this.state.num}</h3>
+        <button className="btn btn-success">Increase</button>
+        <br />
+        <br />
+        <button className="btn btn-warning">Descrease</button>
       </div>
-    );
+    );    
   }
 }
 
